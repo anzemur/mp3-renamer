35255acc44e2d78d2fe561fb657d357691fea89a
Anze Mur <am3158@student.uni-lj.si>
2017-11-16 01:58:25 +0100

    Update preimenujMP3.c.c

--- preimenujMP3.c.c
+++ preimenujMP3.c.c
@@ -5,48 +5,31 @@
 int main(int argc, char* argv[]){
     
     FILE* f;
-    
+ 
     for(int i = 1; i < argc; i++){
- 		f = fopen(argv[i], "r");	
- 		
- 		char *ime = malloc(30*sizeof(char));
+	    
+ 		f = fopen(argv[i], "r");		
+ 		char *song_name = malloc(30*sizeof(char));
  		
  		char *dump = malloc(3*sizeof(char));
  		fseek(f, -128 ,SEEK_END );
  		
- 		for(int j= 0; j < 3; j++){
- 			fscanf(f, "%c", &dump[j]);
+ 		for(int j= 0; j < 3; j++) fscanf(f, "%c", &dump[j]);
+ 		for(int j= 0; j < 30; j++)fscanf(f, "%c", &song_name[j]);
  		
- 		}
- 		for(int j= 0; j < 30; j++){
- 			fscanf(f, "%c", &ime[j]);
- 		}
+ 			
+ 		char *performer_name = malloc(30*sizeof(char));
  		
+ 		for(int j= 0; j < 30; j++) fscanf(f, "%c", &performer_name[j]);
  		
- 		
- 		
- 		
- 		
- 		char *izvajalec = malloc(30*sizeof(char));
- 		
- 		for(int j= 0; j < 30; j++){
- 			fscanf(f, "%c", &izvajalec[j]);
- 		}
- 		
-
 		char* new_name = malloc(65*sizeof(char));
 		
-		strcpy(new_name, izvajalec);
+		strcpy(new_name, performer_name);
 		strcat(new_name, " - ");
-		strcat(new_name, ime);
+		strcat(new_name, song_name);
 		
 		int ren = rename(argv[i], new_name);
 		
- 		printf("IZVAJALEC + IME: %s - %s\n", izvajalec, ime);
- 		printf("IZVAJALEC + IME ZDRUZENO: %s\n", new_name);
- 		printf("/n");
- 		
- 		
         
     
     }
